80ea803435b3d4d265ca66ce401e32540e7d6f59
Timur <122035339+Pirot1@users.noreply.github.com>
2026-04-13 22:11:12 +0500

    Barely working

--- pkg/parser/test.go
+++ pkg/parser/test.go
@@ -1,34 +1,58 @@
 package parser
 
 import (
-	"fmt"
 	"log"
+	"time"
+
+	"AutoGoogleDocs/pkg/ai"
 
 	"github.com/go-rod/rod"
 )
 
-func Start_quiz(page *rod.Page, line string) (data string) {
+func Start_quiz(page *rod.Page, line string) {
 	log.Println("Start solving quiz...")
-	page.MustElementX(`//input[@type="text"][1]`).MustInput(line)
-	els := page.MustElementsX(`//div[@role="listitem"]`)
-	data = ""
-	for _, el := range els {
-		text := el.MustText()
-		data = fmt.Sprintf("%s\n%s", data, text)
+	time.Sleep(500 * time.Millisecond)
+	page.MustElementX(`//input[@type="text"]`).MustClick().MustInput(line)
+	step := 5
+	elements := page.MustElementsX(`//div[@role="listitem"]`)
+	var totalAns []int
+	for i := 0; i < len(elements); i += step {
+		end := i + step
+		if end > len(elements) {
+			end = len(elements)
+		}
+		chunk := elements[i:end]
+		var questionBlock string
+		for _, el := range chunk {
+			questionBlock += el.MustText() + "\n"
+		}
+		answer, err := ai.AskAI(questionBlock)
+		if err != nil {
+			log.Println("Error with loading AI:", err)
+			continue
+		}
+		for j := 0; j < len(answer); j++ {
+			totalAns = append(totalAns, answer[j])
+		}
 	}
-	return data
+	Solve_quiz(page, totalAns)
 }
 
 func Solve_quiz(page *rod.Page, answer []int) {
-	elements := page.MustElementsX(`//div[@class="Qr7Oae"]`)
-
-	for i := 1; i <= len(elements); i++ {
-		answers := elements[i].MustElementsX(`//div[@class="nWQGrd zwllIb"]`)
-		for id, ans := range answers {
-			if id == answer[i-1] {
-				ans.MustElementX(`//div[@class="d7L4fc bJNwt  FXLARc aomaEc ECvBRb"]`).MustClick()
-				break
-			}
+	questions := page.MustElementsX(`//div[@role="listitem"]`)
+	for i, questionEl := range questions {
+		if i >= len(answer) {
+			log.Printf("Caution: Answers is less than questions. Stop on question %d", i+1)
+			break
+		}
+		options := questionEl.MustElementsX(`.//div[@class="nWQGrd zwllIb"]`)
+		targetIndex := answer[i]
+		if targetIndex >= 0 && targetIndex < len(options) {
+			btn := options[targetIndex].MustElementX(`.//div[@class="d7L4fc bJNwt FXLARc aomaEc ECvBRb"]`)
+			btn.MustClick()
+			log.Printf("Question %d: the answer is %d", i+1, targetIndex)
+		} else {
+			log.Printf("ERROR: index %d is out of range (with length %d)", targetIndex, len(options))
 		}
 	}
 	log.Println("Test is done!")
